38e521a61a684fe1499dbe289600a3e9f1a7439d
BuildTools <unconfigured@null.spigotmc.org>
2023-06-09 00:09:12 -0300

    ADD CriancaAtividade.cs method

---
+++ API/ProjetoDeModelagemDeSistemas/Domain/Entities/CriancaAtividade.cs
@@ -0,0 +1,21 @@
+namespace Domain.Entities;
+
+public class CriancaAtividade : Entity
+{
+
+    public Crianca IdCrianca { get; set; }
+    public Atividade IdAtividade { get; set; }
+    public bool Realizado { get; set; }
+
+    public CriancaAtividade(int idCrianca, Atividade idAtividade, bool realizado)
+    {
+        IdCrianca.Id = idCrianca;
+        IdAtividade = idAtividade;
+        Realizado = realizado;
+    }
+
+    public void finalizarTarefa()
+    {
+        Realizado = true;
+    }
+}
